0f624f19ca3c5603c248469568bcd2ff2cd128fc
Evandson César <evandson.cesar@gmail.com>
2023-03-30 22:40:15 -0300

    Estabelecendo estilos, organizando o player de vídeo, e colocando o primeiro vídeo para teste

---
+++ src/pages/Video.js
@@ -0,0 +1,38 @@
+import React, { useRef, useState} from "react";
+import "./Video.css";
+
+
+function Video() {
+
+    const videoRef = useRef(null)
+    const [play, setPlay] = useState(false)
+
+      function handdleStart(){
+        if(!play){
+          videoRef.current.play()
+          setPlay(true)
+        }
+        else{
+          videoRef.current.pause()
+          setPlay(false)
+        }
+        
+      }
+
+
+  return (
+    <div className="video">
+         <video className="video__player"
+          ref={videoRef}
+          onClick={handdleStart}
+          loop
+
+         src="https://cdn.discordapp.com/attachments/1090396071261372491/1091167951245295667/brecker2.mp4" 
+       >
+
+       </video>
+    </div>
+  );
+}
+
+export default Video;
