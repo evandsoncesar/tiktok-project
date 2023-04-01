47660d629ce40d9939100e9cec23f18ff889b277
Evandson César <evandson.cesar@gmail.com>
2023-03-31 22:09:00 -0300

    feito o texto, ícones e animações do Footer

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,36 +1,32 @@
-import React, { useRef, useState} from "react";
+import React, { useRef, useState } from "react";
+import VideoFooter from "./components/footer/VideoFooter";
 import "./Video.css";
 
-
 function Video() {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
 
-    const videoRef = useRef(null)
-    const [play, setPlay] = useState(false)
-
-      function handdleStart(){
-        if(!play){
-          videoRef.current.play()
-          setPlay(true)
-        }
-        else{
-          videoRef.current.pause()
-          setPlay(false)
-        }
-        
-      }
-
+  function handdleStart() {
+    if (!play) {
+      videoRef.current.play();
+      setPlay(true);
+    } else {
+      videoRef.current.pause();
+      setPlay(false);
+    }
+  }
 
   return (
     <div className="video">
-         <video className="video__player"
-          ref={videoRef}
-          onClick={handdleStart}
-          loop
-
-         src="https://cdn.discordapp.com/attachments/1090396071261372491/1091167951245295667/brecker2.mp4" 
-       >
-
-       </video>
+      <video
+        className="video__player"
+        ref={videoRef}
+        onClick={handdleStart}
+        loop
+        src="https://cdn.discordapp.com/attachments/1090396071261372491/1091167951245295667/brecker2.mp4"
+      ></video>
+      {/* Side bar */}
+      <VideoFooter />
     </div>
   );
 }
